486620bba4b1016b4edf2a0c93f2cef008d20bed
Alvin <alvinraditya101@gmail.com>
2026-07-28 16:18:38 +0700

    fix: externalize jwks-rsa and jose in experimental serverComponentsExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["firebase-admin"],
+    serverComponentsExternalPackages: ["firebase-admin", "jwks-rsa", "jose"],
   },
 };
 
